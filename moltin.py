2d76287306085e8801b4c04dfeb884ee36714dc0
canto9112 <cantoweb112@gmail.com>
2021-05-03 20:17:34 +0300

    Вынес лишние переменные внутрь функций

--- moltin.py
+++ moltin.py
@@ -3,22 +3,22 @@ from environs import Env
 from pprint import pprint
 
 
-def get_access_token(url, client_id, client_secret):
+def get_access_token(client_id, client_secret):
     data = {
         'client_id': client_id,
         'client_secret': client_secret,
         'grant_type': 'client_credentials'
     }
-    response = requests.post(url, data=data)
+    response = requests.post('https://api.moltin.com/oauth/access_token', data=data)
     response.raise_for_status()
     response = response.json()
     access_token = response['access_token']
     return access_token
 
 
-def get_all_products(url, token):
+def get_all_products(token):
     headers = {'Authorization': f'Bearer {token}'}
-    response = requests.get(url, headers=headers)
+    response = requests.get('https://api.moltin.com/v2/products', headers=headers)
     response.raise_for_status()
     response = response.json()
     return response['data']
@@ -73,17 +73,14 @@ def main():
     env = Env()
     env.read_env()
 
-    access_token_url = 'https://api.moltin.com/oauth/access_token'
-    all_products_url = 'https://api.moltin.com/v2/products'
-
     client_id = env('MULTIN_CLIENT_ID')
     client_secret = env('MULTIN_CLIENT_SECRET')
 
-    access_token = get_access_token(access_token_url, client_id, client_secret)
-    all_products = get_all_products(all_products_url, access_token)
+    access_token = get_access_token(client_id, client_secret)
+    all_products = get_all_products(access_token)
 
-    for prosuct in all_products:
-        product_id = get_id_product(prosuct)
+    for product in all_products:
+        product_id = get_id_product(product)
         # add_product = add_product_to_cart(access_token, product_id, '3456546', 2)
         # cart = get_cart(access_token, '3456546')
         # pprint(cart)

667a7e9ac419beed274bfdf39183b0d2a9bc12fe
canto9112 <cantoweb112@gmail.com>
2021-05-09 08:48:20 +0300

    Написали функцию обновления корзины

--- telegram_bot.py
+++ telegram_bot.py
@@ -81,14 +81,48 @@ def cart(bot, update, products, access_token):
     elif query.data:
         print(query.data)
         moltin.delete_product_in_cart(access_token, query.message.chat_id, query.data)
+        # del_old_message(bot, update)
+        update_cart(bot, update, access_token)
         return "HANDLE_CART"
 
 
-def send_mail(bot, update):
-    # query = update.callback_query
-    # update.message.reply_text('Вы ввели:')
-    # bot.send_message(chat_id=query.message.chat_id, text='привет Саша')
+def update_cart(bot, update, access_token):
+    query = update.callback_query
+    button_menu = [InlineKeyboardButton("Меню", callback_data="Меню")]
+    button_pay = [InlineKeyboardButton("Оплатить", callback_data="Оплатить")]
 
+    cart_items = moltin.get_cart_items(access_token, query.message.chat_id)
+    products_in_cart = []
+    keyboard = []
+    for product_in_cart in cart_items['data']:
+        product_name = product_in_cart['name']
+        button = [InlineKeyboardButton(f'Убрать из корзины {product_name}', callback_data=product_in_cart['id'])]
+        keyboard.append(button)
+
+        description = product_in_cart['description']
+        price = product_in_cart['meta']['display_price']['with_tax']['unit']['formatted']
+        quantity = product_in_cart['quantity']
+        all_price = product_in_cart['meta']['display_price']['with_tax']['value']['formatted']
+
+        products_in_cart.append(
+            f'{product_name}\n{description}\n{price}per kg\n{quantity}kg in cart for {all_price}\n\n')
+
+    keyboard.append(button_menu)
+    keyboard.append(button_pay)
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    myString = ''.join(products_in_cart)
+    print(myString)
+    print('============')
+    del_old_message(bot, update)
+
+    cart = moltin.get_cart(access_token, query.message.chat_id)
+    total_price = cart['data']['meta']['display_price']['with_tax']['formatted']
+    bot.send_message(chat_id=query.message.chat_id, text=f'{myString}\n'
+                                                         f'Всего на сумму:\n'
+                                                         f'{total_price}', reply_markup=reply_markup)
+
+
+def send_mail(bot, update):
     users_reply = update.message.text
     update.message.reply_text(f'Вы отправили почту вот эту - {users_reply}')
 
@@ -106,38 +140,7 @@ def handle_description(bot, update, products, access_token):
         return 'HANDLE_MENU'
 
     elif button == 'Корзина':
-        button_menu = [InlineKeyboardButton("Меню", callback_data="Меню")]
-        button_pay = [InlineKeyboardButton("Оплатить", callback_data="Оплатить")]
-
-        cart_items = moltin.get_cart_items(access_token, query.message.chat_id)
-        products_in_cart = []
-        keyboard = []
-        for product_in_cart in cart_items['data']:
-
-            product_name = product_in_cart['name']
-            button = [InlineKeyboardButton(f'Убрать из корзины {product_name}', callback_data=product_in_cart['id'])]
-            keyboard.append(button)
-
-            description = product_in_cart['description']
-            price = product_in_cart['meta']['display_price']['with_tax']['unit']['formatted']
-            quantity = product_in_cart['quantity']
-            all_price = product_in_cart['meta']['display_price']['with_tax']['value']['formatted']
-
-            products_in_cart.append(f'{product_name}\n{description}\n{price}per kg\n{quantity}kg in cart for {all_price}\n\n')
-
-        keyboard.append(button_menu)
-        keyboard.append(button_pay)
-        reply_markup = InlineKeyboardMarkup(keyboard)
-        myString = ''.join(products_in_cart)
-        print(myString)
-        print('============')
-        del_old_message(bot, update)
-
-        cart = moltin.get_cart(access_token, query.message.chat_id)
-        total_price = cart['data']['meta']['display_price']['with_tax']['formatted']
-        bot.send_message(chat_id=query.message.chat_id, text=f'{myString}\n'
-                                                             f'Всего на сумму:\n'
-                                                             f'{total_price}', reply_markup=reply_markup)
+        update_cart(bot, update, access_token)
         return "HANDLE_CART"
 
     elif button:
